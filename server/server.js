ca109d4e0e7a720831596404fedfce488c93fac6
camj10 <camilajisel06@gmail.com>
2023-03-25 12:12:30 -0300

    fix cors error

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const puerto = process.env.PUERTO;
 require('./config/mongoose.config');
 app.use(cookieParser());
 app.use(cors({credentials: true, origin: 'http://localhost:3000',exposedHeaders:['kd_token']}));
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 require('./routes/usuarios.routes')(app);
